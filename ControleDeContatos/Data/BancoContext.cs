a122da2abdc65f3b15cbd9d8443061f630a7c72c
thiag <thiag@LAPTOP-I1GJ06OO>
2023-08-09 21:20:44 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeContatos/Data/BancoContext.cs
@@ -0,0 +1,16 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
+
+namespace ControleDeContatos.Data
+{
+    public class BancoContext: DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        { 
+        }
+
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
---
+++ ControleDeContatos/Views/Startup.cs
@@ -0,0 +1,6 @@
+﻿namespace ControleDeContatos.Views
+{
+    public class Startup
+    {
+    }
+}
